e5c54f83a4f794935d16359e8700072a38729090
isaac irvine <isaac564465@gmail.com>
2023-01-16 21:52:27 +1300

    Generates nether too now. Sets world boarder

--- src/main/java/pregen/Pregen.java
+++ src/main/java/pregen/Pregen.java
@@ -1,6 +1,6 @@
 package pregen;
 
-import org.bukkit.event.EventHandler;
+import org.bukkit.World;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -9,14 +9,33 @@ import org.popcraft.chunky.api.ChunkyAPI;
 
 public class Pregen extends JavaPlugin implements Listener {
 
+    private static final String[] WORLDS = {"world", "world_nether"};
+    private static final int WORLD_RADIUS = 2500; // use a much lower number for testing. e.g. 256
+
+    private int worldsIndex = 0;
+
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        startNextWorld();
+        for (World world : getServer().getWorlds()) {
+            world.getWorldBorder().setSize(WORLD_RADIUS);
+        }
+    }
+
+    private void startNextWorld() {
         new BukkitRunnable() {
             @Override
             public void run() {
                 ChunkyAPI chunky = getServer().getServicesManager().load(ChunkyAPI.class);
-                chunky.startTask("world", "square", 0, 0, 2624, 2624, "concentric");
-                chunky.onGenerationComplete(event -> getServer().shutdown());
+                int index = worldsIndex++;
+                chunky.startTask(WORLDS[index], "square", 0, 0, WORLD_RADIUS + 128, WORLD_RADIUS + 128, "concentric");
+                chunky.onGenerationComplete(event -> {
+                    if (worldsIndex >= WORLDS.length) {
+                        getServer().shutdown();
+                    } else {
+                        startNextWorld();
+                    }
+                });
             }
         }.runTaskLater(this, 1);
     }
