43abd17193a5a9b6cf95c6f1b63a08857035d4b1
Isaac Irvine <18374196+Isaac-Irvine@users.noreply.github.com>
2023-01-25 15:34:51 +1300

    Update Pregen.java

--- src/main/java/pregen/Pregen.java
+++ src/main/java/pregen/Pregen.java
@@ -10,7 +10,8 @@ import org.popcraft.chunky.api.ChunkyAPI;
 public class Pregen extends JavaPlugin implements Listener {
 
     private static final String[] WORLDS = {"world", "world_nether"};
-    private static final int WORLD_RADIUS = 2500; // use a much lower number for testing. e.g. 256
+    private static final int WORLD_RADIUS = 2850; // use a much lower number for testing. e.g. 256
+    private static final int WORLD_RENDER_RADIUS = 2000; // use a much lower number for testing. e.g. 256
 
     private int worldsIndex = 0;
 
@@ -28,7 +29,7 @@ public class Pregen extends JavaPlugin implements Listener {
             public void run() {
                 ChunkyAPI chunky = getServer().getServicesManager().load(ChunkyAPI.class);
                 int index = worldsIndex++;
-                chunky.startTask(WORLDS[index], "square", 0, 0, WORLD_RADIUS + 128, WORLD_RADIUS + 128, "concentric");
+                chunky.startTask(WORLDS[index], "circle", 0, 0, WORLD_RENDER_RADIUS, WORLD_RENDER_RADIUS, "concentric");
                 chunky.onGenerationComplete(event -> {
                     if (worldsIndex >= WORLDS.length) {
                         getServer().shutdown();
